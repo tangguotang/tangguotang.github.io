6d1c557225cd00c106773285cbe4083be2f92662
tangguotang <38718217+tangguotang@users.noreply.github.com>
2019-08-06 18:15:41 +0800

    屏蔽D3.JS

--- web/index.js
+++ web/index.js
@@ -10,7 +10,7 @@ window.screenOrientation = "sensor_portrait";
 //-----libs-begin-----
 loadLib("libs/laya.core.js");
 loadLib("libs/laya.ui.js");
-loadLib("libs/laya.d3.js");
+//loadLib("libs/laya.d3.js");
 loadLib("libs/laya.ani.js");
 //-----libs-end-------
 loadLib("js/SpineDemo.js");
